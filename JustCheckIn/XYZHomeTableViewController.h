c0530e74bbae06edcfbcb7f39278e5c39b6bb160
ccweaver <ccweaver@princeton.edu>
2014-07-20 18:18:19 -0400

    setup los storyboard

---
+++ JustCheckIn/XYZHomeTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYZHomeTableViewController.h
+//  JustCheckIn
+//
+//  Created by Campbell Weaver on 7/20/14.
+//  Copyright (c) 2014 Campbell Weaver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZHomeTableViewController : UITableViewController
+
+@end
