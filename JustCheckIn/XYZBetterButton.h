efae3febef073c74e237f68f938a7c48b4bb2e24
ccweaver <ccweaver@princeton.edu>
2014-07-21 10:03:36 -0400

    login scene, create account scene and my checkins

---
+++ JustCheckIn/XYZBetterButton.h
@@ -0,0 +1,13 @@
+//
+//  XYZBetterButton.h
+//  JustCheckIn
+//
+//  Created by Campbell Weaver on 7/20/14.
+//  Copyright (c) 2014 Campbell Weaver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZBetterButton : UIButton
+
+@end
---
+++ JustCheckIn/XYZBetterButton.m
@@ -0,0 +1,31 @@
+//
+//  XYZBetterButton.m
+//  JustCheckIn
+//
+//  Created by Campbell Weaver on 7/20/14.
+//  Copyright (c) 2014 Campbell Weaver. All rights reserved.
+//
+
+#import "XYZBetterButton.h"
+
+@implementation XYZBetterButton
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
